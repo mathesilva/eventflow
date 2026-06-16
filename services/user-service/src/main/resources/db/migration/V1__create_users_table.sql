4e0d82bd44b397af11a5fdf4a12890618bea6ff0
Matheus Faias <faiaxx@MacBook-Air-de-Matheus.local>
2026-06-16 16:07:13 -0300

    feat: (user-service): criacao flyway migration

---
+++ services/user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    dataCriacao TIMESTAMP NOT NULL
+);
