0e6dcfdc3bc5b001a4a70711edd8b362159f3ea9
Matheus Faias <faiaxx@MacBook-Air-de-Matheus.local>
2026-06-24 12:26:49 -0300

    feat: criando migration entity flyway, validando db-event, testanto actuator, criando prepersist e preupdate

--- services/event-service/src/main/java/com/eventflow/eventservice/domain/entity/Event.java
+++ services/event-service/src/main/java/com/eventflow/eventservice/domain/entity/Event.java
@@ -20,15 +20,34 @@ public class Event {
     private String titulo;
     private String descricao;
     private String location;
+
+    @Column(name = "event_date")
     private LocalDateTime eventDate;
     private Integer totalTickets;
+
+    @Column(name = "available_tickets")
     private Integer ticketsDisponiveis;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
     private EventStatus status;
 
+    @Column(name = "organizer_id")
     private UUID organizerID;
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
 
 }
